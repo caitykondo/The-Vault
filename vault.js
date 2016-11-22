37bb69e5953dff65cf8d100f28fa3ccff90626ed
caitypizza <caitykondo@gmail.com>
2016-11-22 09:53:11 -1000

    created vault with setValue and getValue stub

--- vault.js
+++ vault.js
@@ -1,4 +1,16 @@
 'use strict';
 module.exports = function() {
-  
+  let vault = function () {
+    let setValue = function (key, value) {
+
+      // body...
+    };
+    let getValue = function (key) {
+      // body...
+    };
+    return {
+      setValue: setValue,
+      getValue: getValue
+    };
+  };
 };
